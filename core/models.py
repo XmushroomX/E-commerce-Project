8bd443421cd2052c703498b5c50188408e21dad8
XmushroomX <dangvm26@gmail.com>
2024-05-14 23:41:19 +0700

    dangvu

--- core/models.py
+++ core/models.py
@@ -1,21 +1,17 @@
 from django.conf import settings
 from django.db import models
 from django.shortcuts import reverse
-
 # Create your models here.
 CATEGORY_CHOICES = (
     ('SB', 'Shirts And Blouses'),
     ('TS', 'T-Shirts'),
     ('SK', 'Skirts')
 )
-
 LABEL_CHOICES = (
     ('S', 'sale'),
     ('N', 'new'),
     ('P', 'promotion')
 )
-
-
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
@@ -24,29 +20,32 @@ class Item(models.Model):
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
     description = models.TextField()
-
     def __str__(self):
         return self.title
-
     def get_absolute_url(self):
         return reverse("core:product", kwargs={
             'slug': self.slug
         })
-
     def get_add_to_cart_url(self):
         return reverse("core:add-to-cart", kwargs={
             'slug': self.slug
         })
 
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
-
-
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
@@ -54,6 +53,5 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
-
     def __str__(self):
         return self.user.username
