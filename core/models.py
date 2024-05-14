1dfea6553a36199b22b25c5c03ebec5c93eb53f7
XmushroomX <dangvm26@gmail.com>
2024-05-14 17:03:12 +0700

    dangvu

--- core/models.py
+++ core/models.py
@@ -8,14 +8,18 @@ CATEGORY_CHOICES = (
     ('TS', 'T-Shirts'),
     ('SK', 'Skirts')
 )
+
 LABEL_CHOICES = (
     ('S', 'sale'),
     ('N', 'new'),
     ('P', 'promotion')
 )
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
@@ -31,8 +35,11 @@ class Item(models.Model):
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
+
+
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
@@ -40,5 +47,6 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
