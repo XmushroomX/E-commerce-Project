3bfb61198cf689a38d9f7c1cec99fa15d905235f
XmushroomX <dangvm26@gmail.com>
2024-05-14 16:55:26 +0700

    dangvu

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 CATEGORY_CHOICES = (
@@ -7,31 +8,31 @@ CATEGORY_CHOICES = (
     ('TS', 'T-Shirts'),
     ('SK', 'Skirts')
 )
-
 LABEL_CHOICES = (
     ('S', 'sale'),
     ('N', 'new'),
     ('P', 'promotion')
 )
-
-
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2, default='default_category')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
 
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.title
-
-
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
@@ -39,6 +40,5 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
-
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Item
-
 # Create your views here.
-
-
 class HomeView(ListView):
     model = Item
     template_name = "index.html"
-
-
 def home(request):
     context = {
         'items': Item.objects.all()
     }
     return render(request, "index.html", context)
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product-detail.html"
+
+
+def products(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "product-detail.html", context)
